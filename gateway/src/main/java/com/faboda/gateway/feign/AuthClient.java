7fcbcf77b993611ff57d27afb84df45b57798bbb
mikechiloane <38855352+mikechiloane@users.noreply.github.com>
2023-07-11 00:07:11 +0200

    Update AuthClient.java
    fix feign to auth-server

--- gateway/src/main/java/com/faboda/gateway/feign/AuthClient.java
+++ gateway/src/main/java/com/faboda/gateway/feign/AuthClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(value = "auth-server", url = "http://localhost:8081")
+@FeignClient(value = "auth-server", url = "https://auth-server-meon.onrender.com")
 public interface AuthClient {
 
     @GetMapping("/auth0/validate")
